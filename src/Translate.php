f536499ae6b97ff7eea6e9e07e5480cb650f49f1
TJ Draper <tj@buzzingpixel.com>
2017-11-27 19:19:23 -0600

    Initial commit

---
+++ src/Translate.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @author TJ Draper <tj@buzzingpixel.com>
+ * @copyright 2017 BuzzingPixel, LLC
+ * @license Apache-2.0
+ */
+
+namespace felicity\translate;
+
+/**
+ * Class Translate
+ */
+class Translate
+{
+}
---
+++ tests/TranslateTest.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @author TJ Draper <tj@buzzingpixel.com>
+ * @copyright 2017 BuzzingPixel, LLC
+ * @license Apache-2.0
+ */
+
+namespace tests;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class TwigTest
+ */
+class TranslateTest extends TestCase
+{
+}
