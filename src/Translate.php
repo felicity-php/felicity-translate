cdf67e34f79aadaf0e6bda9f2445f8a6cbd52822
TJ Draper <tj@buzzingpixel.com>
2017-11-27 19:48:27 -0600

    Add translation capabilities

--- src/Translate.php
+++ src/Translate.php
@@ -8,9 +8,65 @@
 
 namespace felicity\translate;
 
+use felicity\config\Config;
+
 /**
  * Class Translate
  */
 class Translate
 {
+    /**
+     * Get's a translation
+     * @param string $category
+     * @param string $key
+     * @param array $vars
+     * @param string $locale
+     * @return string
+     */
+    public static function get(
+        string $category,
+        string $key,
+        array $vars = [],
+        string $locale = ''
+    ) : string {
+        $fallBackLocale = Config::get('lang.defaultLocale', 'en');
+
+        if (! $locale) {
+            $locale = Config::get(
+                'lang.locale',
+                Config::get('lang.defaultLocale', 'en')
+            );
+        }
+
+        $fullKey = "lang.translations.{$locale}.{$category}.{$key}";
+        $fallBackKey = "lang.translations.{$fallBackLocale}.{$category}.{$key}";
+
+        $tr = Config::get($fullKey, Config::get($fallBackKey));
+
+        if (! $tr || ! $vars) {
+            return $tr;
+        }
+
+        $tr = strtr($tr, $vars);
+
+        return $tr;
+    }
+
+
+    /**
+     * Get's a translation
+     * @param string $category
+     * @param string $key
+     * @param array $vars
+     * @param string $locale
+     * @return string
+     */
+    public function getTranslation(
+        string $category,
+        string $key,
+        array $vars = [],
+        string $locale = ''
+    ) : string {
+        return self::get($category, $key, $vars, $locale);
+    }
 }
--- tests/TranslateTest.php
+++ tests/TranslateTest.php
@@ -8,11 +8,87 @@
 
 namespace tests;
 
+use felicity\config\Config;
 use PHPUnit\Framework\TestCase;
+use felicity\translate\Translate;
 
 /**
  * Class TwigTest
  */
 class TranslateTest extends TestCase
 {
+    public function testTranslate()
+    {
+        Config::set('lang.translations.en.testing', [
+            'testVar' => 'en test value',
+            'testVar2' => 'en test value 2',
+        ]);
+
+        Config::set('lang.translations.fr.testing', [
+            'testVar' => 'fr test value',
+            'testVar2' => 'fr test value 2',
+        ]);
+
+        Config::set('lang.translations.de.testing', [
+            'testVar' => 'de test value',
+            'testVar2' => 'de test value 2',
+        ]);
+
+        self::assertEquals(
+            'en test value',
+            Translate::get('testing', 'testVar')
+        );
+
+        self::assertEquals(
+            'en test value 2',
+            Translate::get('testing', 'testVar2')
+        );
+
+        Config::set('lang.defaultLocale', 'fr');
+
+        self::assertEquals(
+            'fr test value',
+            Translate::get('testing', 'testVar')
+        );
+
+        self::assertEquals(
+            'fr test value 2',
+            Translate::get('testing', 'testVar2')
+        );
+
+        Config::set('lang.locale', 'de');
+
+        self::assertEquals(
+            'de test value',
+            Translate::get('testing', 'testVar')
+        );
+
+        self::assertEquals(
+            'de test value 2',
+            Translate::get('testing', 'testVar2')
+        );
+
+        Config::set('lang.translations.en.testing', [
+            'testVar' => 'en test value {{innerVar}}',
+        ]);
+
+        self::assertEquals(
+            'en test value innerVal',
+            Translate::get(
+                'testing',
+                'testVar',
+                [
+                    '{{innerVar}}' => 'innerVal'
+                ],
+                'en'
+            )
+        );
+
+        Config::set('lang.locale', 'asdf');
+
+        self::assertEquals(
+            'fr test value',
+            Translate::get('testing', 'testVar')
+        );
+    }
 }
